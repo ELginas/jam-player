75c13d865951a9c058d922f1a9f3b304a6d25a4c
ELginas <gintaras.z123@yahoo.com>
2025-03-20 07:59:10 +0200

    init commit

---
+++ src/background.js
@@ -0,0 +1,7 @@
+// browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   console.log("My extension is now fully loaded!");
+//   result = 1;
+//   sendResponse(result);
+// });
+
+console.log("Initialization phase");
