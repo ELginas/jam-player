549402f5d1a3a4db0a57ad264449ddf9850490ca
ELginas <gintaras.z123@yahoo.com>
2025-03-20 18:43:02 +0200

    Persistance and prototype queue preview

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,12 @@
 <script>
   import "./App.css";
+
+  const queue = browser.runtime.sendMessage({
+    type: "getQueue",
+  });
 </script>
 
+{#await queue then queue}
+  <span>{JSON.stringify(queue)}</span>
+{/await}
 <div class="p-2 bg-orange-200">This is example</div>
--- src/background.js
+++ src/background.js
@@ -1,21 +1,42 @@
-const queue = [];
+async function getStorageItem(key, _default) {
+  const data = await browser.storage.local.get(key);
+  if (data === undefined || data[key] === undefined) {
+    return _default;
+  }
+  const entry = data[key];
+  return JSON.parse(entry);
+}
 
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log(`Message received: ${message}`);
-  if (message.type === "toggleAddQueue") {
-    if (queue.includes(message.item)) {
-      console.log(`Removed "${message.item}" from the queue`);
-      queue.splice(queue.indexOf(message.item));
-      sendResponse(false);
-    } else {
-      console.log(`Add "${message.item}" to the queue`);
-      queue.push(message.item);
-      sendResponse(true);
+async function setStorageItem(key, value) {
+  await browser.storage.local.set({
+    [key]: JSON.stringify(value),
+  });
+}
+
+(async () => {
+  const queue = await getStorageItem("queue", []);
+
+  browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(`Message received: ${message}`);
+    if (message.type === "toggleAddQueue") {
+      if (queue.includes(message.item)) {
+        console.log(`Removed "${message.item}" from the queue`);
+        queue.splice(queue.indexOf(message.item));
+        sendResponse(false);
+      } else {
+        console.log(`Add "${message.item}" to the queue`);
+        queue.push(message.item);
+        sendResponse(true);
+      }
+      setStorageItem("queue", queue);
     }
-  }
-  if (message.type === "isInQueue") {
-    sendResponse(queue.includes(message.item));
-  }
-});
+    if (message.type === "isInQueue") {
+      sendResponse(queue.includes(message.item));
+    }
+    if (message.type === "getQueue") {
+      sendResponse(queue);
+    }
+  });
 
-console.log("Jam player background script initialized");
+  console.log("Jam player background script initialized");
+})();
