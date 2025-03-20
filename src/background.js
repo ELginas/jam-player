e3871c4f6da518fdcc8790d62af2cda6b50ae82d
ELginas <gintaras.z123@yahoo.com>
2025-03-20 17:30:41 +0200

    Appending into a queue

--- src/background.js
+++ src/background.js
@@ -1,7 +1,21 @@
-// browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//   console.log("My extension is now fully loaded!");
-//   result = 1;
-//   sendResponse(result);
-// });
+const queue = [];
 
-console.log("Initialization phase");
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log(`Message received: ${message}`);
+  if (message.type === "toggleAddQueue") {
+    if (queue.includes(message.item)) {
+      console.log(`Removed "${message.item}" from the queue`);
+      queue.splice(queue.indexOf(message.item));
+      sendResponse(false);
+    } else {
+      console.log(`Add "${message.item}" to the queue`);
+      queue.push(message.item);
+      sendResponse(true);
+    }
+  }
+  if (message.type === "isInQueue") {
+    sendResponse(queue.includes(message.item));
+  }
+});
+
+console.log("Jam player background script initialized");
--- src/content.js
+++ src/content.js
@@ -1,5 +1,4 @@
-console.log("This is loaded! index_game_cell_widget game_cell");
-console.log(document.readyState);
+console.log("Jam player content script initialized");
 
 function hasButton(element) {
   return element.querySelector(".jambutton") !== null;
@@ -9,45 +8,79 @@ function canAddButton(element) {
   return element.querySelector(".fading_data.cell_tools") !== null;
 }
 
-function addButton(element) {
+function gameIdFromHref(href) {
+  const arr = href.split("/");
+  return arr[arr.length - 1];
+}
+
+function queueTypeIcon(isInQueue) {
+  if (isInQueue) {
+    return browser.runtime.getURL("assets/RemoveFromQueue.svg");
+  } else {
+    return browser.runtime.getURL("assets/AddToQueue.svg");
+  }
+}
+
+async function addButton(element) {
   const container = element.querySelector(".fading_data.cell_tools");
+  const gameThumb = element.querySelector(".game_thumb");
+  const gameId = gameIdFromHref(gameThumb.href);
   const newElement = document.createElement("div");
-  newElement.onclick = () => {
-    console.log("Helloo");
-  };
-  newElement.classList.add("add_to_collection_btn");
+
+  const image = document.createElement("img");
   newElement.classList.add("jambutton");
+  newElement.appendChild(image);
+  container.appendChild(newElement);
+
+  // Must appear after creating elements because otherwise you can have multiple button creations in process
+  let isInQueue = await browser.runtime.sendMessage({
+    type: "toggleAddQueue",
+    item: gameId,
+  });
+  //   let isInQueue = false;
+  console.log(isInQueue);
+
+  newElement.classList.add("add_to_collection_btn");
   newElement.style.paddingLeft = "2px";
   newElement.style.paddingRight = "2px";
-  const image = document.createElement("img");
-  const url = browser.runtime.getURL("assets/AddToQueue.svg");
+  const url = queueTypeIcon(isInQueue);
   image.setAttribute("src", url);
   image.style.display = "block";
   image.style.position = "relative";
   image.style.opacity = "100%";
   image.style.width = "24px";
   image.style.height = "24px";
-  newElement.appendChild(image);
-  container.appendChild(newElement);
+  newElement.onclick = async () => {
+    isInQueue = await browser.runtime.sendMessage({
+      type: "toggleAddQueue",
+      item: gameId,
+    });
+    console.log("sett");
+    const url = queueTypeIcon(isInQueue);
+    image.setAttribute("src", url);
+  };
 }
 
-function scanAddElements() {
+async function scanAddElements() {
   const elements = document.querySelectorAll(
     ".index_game_cell_widget.game_cell"
   );
 
   for (const element of elements) {
+    // TODO: concurrency
     if (canAddButton(element) && !hasButton(element)) {
-      addButton(element);
+      await addButton(element);
     }
   }
 }
 
 const mainContainer = document.querySelector(".primary_column");
-const observer = new MutationObserver((mutationsList, observer) => {
-  scanAddElements();
+const observer = new MutationObserver(async (mutationsList, observer) => {
+  await scanAddElements();
 });
 const config = { childList: true, subtree: true };
 observer.observe(mainContainer, config);
 
-scanAddElements();
+(async () => {
+  scanAddElements();
+})();
