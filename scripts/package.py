fabda25f664ffb33465f239d664c8528bc0cc9eb
ELginas <gintaras.z123@yahoo.com>
2025-03-24 19:58:26 +0200

    Packaging script

---
+++ scripts/package.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import os
+from pathlib import Path
+import re
+import shutil
+import subprocess
+
+project_dir = Path(__file__).parent.parent
+artifacts_dir = project_dir / 'web-ext-artifacts'
+
+
+def get_file_version(filename):
+    pattern = r'jam_player-([0-9]+.[0-9]+.[0-9]+).zip'
+    res = re.match(pattern, filename)
+    if not res:
+        return
+    version = res.group(1)
+    return version
+
+
+def move_output(suffix):
+    for filename in os.listdir(artifacts_dir):
+        full_path = os.path.join(artifacts_dir, filename)
+        if os.path.isfile(full_path):
+            version = get_file_version(filename)
+            if not version:
+                continue
+            new_path = artifacts_dir / f'jam_player-{version}-{suffix}.zip'
+            shutil.move(full_path, new_path)
+
+
+subprocess.run(['pnpm', 'package:firefox'], check=True)
+move_output('firefox')
+subprocess.run(['pnpm', 'package:chrome'], check=True)
+move_output('chrome')
